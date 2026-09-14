7e9dca4f92c9de213a4a46343c2646130c7d228c
Son-Gyeongi <thsruddl77@gmail.com>
2026-09-14 21:40:15 +0900

    [필수실습 12-2] '*'출력 놀이 (삼각형 기본)

---
+++ src/section12/Exec122.java
@@ -0,0 +1,13 @@
+package section12;
+
+public class Exec122 {
+    static void main() {
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println('\n');
+        }
+    }
+}
